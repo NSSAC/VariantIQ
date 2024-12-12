d39fc7bbbc4aab9e450aaecb1ac0341c87594844
Dustin Machi <dm8qs@virginia.edu>
2024-12-12 09:56:37 -0500

    first ver of pipeline runner

--- variantiq-benchmark/src/benchmark.py
+++ variantiq-benchmark/src/benchmark.py
@@ -27,17 +27,21 @@ def default_action(
         print(f"Reading Sample CSV: {sample_file}")
         csv_reader = csv.DictReader(f, delimiter=',')
         for row in csv_reader:
-            run_varifier(f"{data_directory}/{row['sample_name']}")
-            # if running vcf tools
-            #     run varifier make_truth_vcf truth_genome.vcf
-            #     run vcftools
+            pass
+            #for each vcf
+                # run normalizer
+                #run_varifier(f"{data_directory}/{row['sample_name']}", vcf file)
+
+            # if running vcfdist
+            #     run varifier make_truth_vcf truth_genome.fasta
+            #     run vcfdist
             #
             # copy results to keep to sample_folder
             # remove intermediate files
 
         # run report generator 
-        
-def run_varifier(sample_folder):
+
+def run_varifier(sample_folder,vcffile):
     print(f"Run Varifier: {sample_folder}")
     cmd = VARIFIER + ["vcf_eval",f"{sample_folder}/truth_genome.fasta",f"{sample_folder}/reference_genome.fasta",f"{sample_folder}/*.vcf",f"{sample_folder}/_varifier"]
     result = subprocess.run(cmd,capture_output=True, text=True)
---
+++ variantiq-pipelines/src/run_pipelines.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python 
+
+#!/usr/bin/env python 
+from cyclopts import App
+from typing import Literal,Union
+import os
+import os.path
+import json
+import subprocess
+import csv
+import requests
+import shutil
+import glob
+
+__dirname__=os.path.dirname(os.path.abspath(__file__))
+print(f"Script Directory: {__dirname__}")
+VARIFIER=["scif","run","varifier"]
+
+benchmark=App()
+
+@benchmark.command
+def list():
+    """
+    List available benchmark pipelines
+    """
+    print("Available Pipelines:")
+    for pipeline in get_pipelines():
+        print(f"- {pipeline}")
+
+@benchmark.command
+def run_benchmark_pipelines(
+    data_directory: str = "/output",
+    sample_file: str = "/output/benchmark_samples.csv",
+    ignore_vcfs: list = []
+):
+    """
+    Run benchmark pipelines over a benchmarking dataset
+    """
+    print("Run Pipelines")
+    print(f"Data Directory: {data_directory} Sample File: {sample_file}")
+    with open(sample_file, 'r') as f:       
+        print(f"Reading Sample CSV: {sample_file}")
+        csv_reader = csv.DictReader(f, delimiter=',')
+        for row in csv_reader:
+            run_pipelines(f"{data_directory}/{row['sample_name']}")
+
+
+def get_pipelines():
+    pipelines=[]
+    for file in glob.glob(f"{__dirname__}/pipelines/*.pipeline.sh"):
+        pipelines.append(os.path.basename(file).split(".",1)[0])
+
+    return pipelines
+
+def run_pipelines(sample_folder):
+    print(f"Run Pipelines: {sample_folder}")
+    
+    for pipeline in get_pipelines():
+        cmd = [f"pipelines/{pipeline}.pipeline.sh",sample_folder]
+        print(f"Command: {cmd} {__dirname__}")
+        result = subprocess.run(cmd,cwd=__dirname__,capture_output=True, text=True)
+        print(f"Results for {pipeline} {sample_folder}:\n{result.stdout}\n{result.stderr}")
+
+benchmark()
