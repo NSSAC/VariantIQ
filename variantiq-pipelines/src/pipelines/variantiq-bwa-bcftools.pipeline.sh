7301b429d3afb1f82da74ad21e27f111d485e90f
chmao1 <>
2025-07-01 14:38:45 -0400

    add nthread option to alignment step

--- variantiq-pipelines/src/pipelines/variantiq-bwa-bcftools.pipeline.sh
+++ variantiq-pipelines/src/pipelines/variantiq-bwa-bcftools.pipeline.sh
@@ -39,7 +39,7 @@ if [ ! -f "${SAMPLE_FOLDER}/${PIPELINE_NAME}.vcf" ]; then
     TRUTH_GENOME=${BUILD_FOLDER}/truth_genome.fasta   
 
     ALIGNER_INSTANCE=_shared/bwa_alignment
-    align-bwa.sh $SAMPLE_FOLDER $ALIGNER_INSTANCE
+    align-bwa.sh $SAMPLE_FOLDER $ALIGNER_INSTANCE $NTHREAD
     ALIGNER_FOLDER=${SAMPLE_FOLDER}/$ALIGNER_INSTANCE
 
     if [ ! -f "${ALIGNER_FOLDER}/aligned_reads.bam" ]; then
